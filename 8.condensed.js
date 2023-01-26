0521d24d4590d6ff94b187cc032b783747eac279
PhilipovPh <philipov.ph@gmail.com>
2023-01-26 15:12:54 +0200

    Update 8.condensed.js

--- 8.condensed.js
+++ 8.condensed.js
@@ -2,13 +2,13 @@ function solve(arr) {
     let currArr = arr;
     while (currArr.length > 1) {
         let newArr = [];
-        for (let i = 0; i < currArr.length; i++) {
+        for (let i = 0; i < currArr.length - 1; i++) {
             let firstElement = currArr[i];
             let secondElement = currArr[i + 1];
             newArr.push(firstElement + secondElement);
         }
         currArr = newArr;
     }
-    console.log(currArr);
+    console.log(`${currArr}`);
 }
-
+solve([2, 10, 3]);
